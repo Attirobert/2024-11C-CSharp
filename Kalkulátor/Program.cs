ae0d28a37231fda18cdca2d9a08941414ee13681
Attirobert <robatti@gmail.com>
2024-10-19 01:40:12 +0200

    Szorzótábla, Kalkulátor

---
+++ Kalkulátor/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region A program feladata
+/* 
+ * A program bekér 
+ *  - egy valós számot
+ *  - egy műveleti jelet a következők közül: + - * / %
+ *  - egy újabb valós számot
+ *  Ezután kiírja a művelet eredményét
+ *  Ezután megkérdezi, hogy akar-e a kezelő újabb számítást végezni, 
+ *      - ha igen, akkor törli a képernyőt és újra kezdi az adatbekérést
+ *      - ha nem, akkor törli a képernyőt és kilép a programbóln
+ *      
+ */
+#endregion Eof A program feladata
+
+namespace Kalkulátor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double szám1 = 0, szám2 = 0, eredmény = double.NaN;
+            char művelet;
+                
+            bool folytatás = true;
+
+            int olvasásSzámláló,
+                maxOlvasás = 5;
+
+            do
+            {
+                olvasásSzámláló = 0;
+
+                Console.WriteLine("Adja meg az első számot!");
+                szám1 = double.Parse(Console.ReadLine());
+
+                do
+                {
+                    Console.WriteLine("Adja meg a műveleti jelet (+ - * / %)!");
+                    művelet = char.Parse(Console.ReadLine());
+
+                    if (++olvasásSzámláló > maxOlvasás)
+                    {
+                        Console.WriteLine("Sajnálom, tovább nem próbálkozhat!");
+                        Environment.Exit(0);
+                    }
+                    
+                }
+                while(művelet != '+' && művelet != '-' && művelet != '*' && művelet != '/' && művelet != '%');
+
+                Console.WriteLine("Adja meg a második számot!");
+                szám2 = double.Parse(Console.ReadLine());
+
+                switch (művelet)
+                {
+                    case '+': eredmény = szám1 + szám2; break;
+                    case '-': eredmény = szám1 - szám2; break;
+                    case '*': eredmény = szám1 * szám2; break;
+                    case '/': eredmény = szám1 / szám2; break;
+                    case '%': eredmény = szám1 % szám2; break;
+                }
+
+                Console.WriteLine($"A művelet eredménye: {eredmény}");
+                Console.WriteLine("Folytatja a számolást (i/n)?");
+
+                folytatás = char.Parse(Console.ReadLine()) == 'i';
+                Console.ReadKey();
+                Console.Clear();
+            } while (folytatás);
+        }
+    }
+}
--- Szorzótábla/Program.cs
+++ Szorzótábla/Program.cs
@@ -14,9 +14,47 @@ using System.Threading.Tasks;
 namespace Szorzótábla
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
+            #region Megoldás
+            // Változók
+            int szám = 0;   // Ebbe olvassuk be a számot
+            bool folytatás = true; // Ezzel jelzem, hogy folytatható-e a játék
+
+            do
+            {
+                // Tájékoztató szöveg a kezelőnek
+                Console.WriteLine("Adjon meg egy egész számot 1 és 9 között!\nHa be akarja fejezni a játékot, akkor 0-t írjon be!");
+
+                // A beolvasást a feltételbe is írhatom.  Figyelni kell a zárójelezésre!
+                if ((szám = int.Parse(Console.ReadLine())) != 0)
+                {
+                    // Ellenőrzöm, hogy megfelelő-e a szám
+                    if (szám >=1 & szám<=9)     // ...ha megfelelő
+                    {
+                        // Ciklusban kiíratom a szorzótáblát
+                        for (int i = 1; i < 9; i++)
+                        {
+                            Console.WriteLine("{0} x {1} = {2}", i, szám, i * szám);
+                        }
+                    }
+                    else    // ...nem megfelelő
+                    {
+                        Console.WriteLine("Hibás számot adott meg!");
+                    }
+
+                }
+                else
+                {
+                    // Kilépek a programból
+                    Console.WriteLine("Köszönöm a játékot");
+                    folytatás = false;
+                }
+                Console.WriteLine("Tovább bármely billentyűvel...");
+                Console.ReadKey();
+            }while(folytatás);
+            #endregion Eof Megoldás
         }
     }
 }
