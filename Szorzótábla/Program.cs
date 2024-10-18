a3e8e01b78e6b93b76e24e941f7a15cc521bc436
Attirobert <karobatti@gmail.com>
2024-10-18 15:37:42 +0200

    Szorzótábla

---
+++ Szorzótábla/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region A program feladata
+/* 
+ * A program bekér egy 1 és 9 közé eső egész számot.  Leellenőrzi, hogy jó számot kapott-e! 
+ * Ezután kiírja a számhoz tartozó szorzótáblát.
+ * Ezt mindaddig ismétli, amíg 0-át nem írok be.
+ */
+#endregion Eof A program feladata
+namespace Szorzótábla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
