1156aac9462a246b659ae6c18fad6d0bb3193ac5
Attirobert <karobatti@gmail.com>
2024-10-21 14:05:45 +0200

    Kő-papír-olló játék

---
+++ KőPapírOlló/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * A klasszikus játék megvalósítása.
+ * A játék addig folytatódik, amíg a játékos "n" jelet ír be.
+ * A játékos választását véletlenszám generátorral állítjuk elő.
+ * Tartsa nyílván a játék állását, amit minden forduló után kiír.
+ */
+namespace KőPapírOlló
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Változók
+            Random r = new Random();    // Random objektum 
+
+            string compVálasztása = "",
+                játékosVálasztása = "";
+
+            int compPontjai = 0,
+                játékosPontjai = 0;
+
+            do
+            {
+                // A játékos választása
+                Console.WriteLine("Válassz! (k/p/o)");
+
+                switch (Console.ReadKey().KeyChar)
+                {
+                    case 'k': játékosVálasztása = "kő"; break;
+                    case 'p': játékosVálasztása = "papír"; break;
+                    case 'o': játékosVálasztása = "olló"; break;
+                    default:
+                        Console.WriteLine("Hibás válasz!");
+                        continue;   // Hibás válasz esetén újra kezdi a ciklust
+                }
+
+                // A gép választása
+                switch (r.Next(0,3)
+                {
+                    case 0: compVálasztása = "kő"; break;
+                    case 1: compVálasztása = "papír"; break;
+                    case 2: compVálasztása = "olló"; break;
+                }
+
+                // A játék kiértékelése
+                if (    // A gép nyerései
+                    (játékosVálasztása == "kő" && compVálasztása == "papír")
+                    || (játékosVálasztása == "papír" && compVálasztása == "olló")
+                    || (játékosVálasztása == "olló" && compVálasztása == "kő")
+                    )
+                {
+                    Console.WriteLine($"A gép nyert! A játék állása:\nSzámítógép: {++compPontjai},\nTe: {játékosPontjai}");
+                }
+                else if (játékosVálasztása == compVálasztása)   // Döntetlen
+                {
+                    Console.WriteLine($"Döntetlen! A játék állása:\nSzámítógép: {compPontjai},\nTe: {játékosPontjai}");
+                }
+                else
+                {   // A játékos nyert
+                    Console.WriteLine($"A játékos nyert! A játék állása:\nSzámítógép: {compPontjai},\nTe: {++játékosPontjai}");
+                }
+
+                // Játék folytatása vagy befejezése
+                Console.WriteLine("Akarsz még játszani? i/n");
+
+                // A Console.ReadKey() ConsoleKeyInfo objektumot ad vissza, amelyben a KeyChar tulajdonság tartalmazza a leütött karaktert.
+                // A true paraméter hatására nem jelenik meg a képernyőn a leütött karakter
+            } while (Console.ReadKey(true).KeyChar == 'n');
+        }
+    }
+}
---
+++ PrePlusz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Annak bemutatása, hogy mi a különbség a műveleti jel változó előtti és utáni használatában
+ * */
+namespace PrePlusz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100,
+                y = 11;
+
+            Console.WriteLine($"1. iratás x = {x}, x++ = {x++}");
+            Console.WriteLine($"2. iratás x = {x}, x++ = {x++}");
+            Console.WriteLine($"1. iratás y = {y}, ++y = {++y}");
+            Console.WriteLine($"2. iratás y = {y}, ++y = {++y}");
+            Console.ReadKey();
+        }
+    }
+}
