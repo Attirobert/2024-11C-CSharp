7b109da038c37e984442800115a45e42bd7561de
Attirobert <robatti@gmail.com>
2024-10-24 20:16:09 +0200

    Hőmérséklet kezdés

---
+++ Hőmérséklet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Az év minden napján feljegyeztük az átlaghőmérsékletet.  Az egyszerűség kedvéért 28 napos hónapokkal számoltunk. Az eredményeket egy mátrixban tároltuk.  A program feladata, hogy kiírja a következőket:
+ *  - Melyik volt az év legmelegebb és leghidegebb napja, hány fokkal?
+ *  - Melyik volt az év legmelegebb és leghidegebb hónapja
+ *  - Volt-e egymás után 5 olyan nap, amikor fagyott?
+ *  
+ *  Megjegyzések:
+ *  - A hőmérsékleteket véletlenszám generátorral állítsuk elő.  Vegyük figyelembe, hogy mik a jellemző hőmérsékletek az egyes hónapokban (pl. nyáron ne legyen -20 fok).
+ *  - A hónapokat névvel írassuk ki, ne számmal.
+ */
+namespace Hőmérséklet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
