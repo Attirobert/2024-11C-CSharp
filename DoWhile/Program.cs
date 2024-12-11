6a0f815f9cb5c0f049d14e59cbf83110701f2eeb
Attirobert <karobatti@gmail.com>
2024-12-11 13:51:13 +0100

    Órai anyag

---
+++ DoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+
+            do
+            {
+                Console.WriteLine("Adjon meg egy egész számot!");
+                szam = Convert.ToInt32(Console.ReadLine());
+                if (szam > 0) Console.WriteLine(" A szám pozitív.");
+                else Console.WriteLine("A szám nem pozitív.");
+
+            } while (szam != 0);
+
+            Console.WriteLine("Köszönöm a játékot!");
+            Console.ReadKey();
+
+        }
+    }
+}
