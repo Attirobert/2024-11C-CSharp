8aa2f96d6643eae257c9ee92def0267a8b86189c
Attirobert <robatti@gmail.com>
2024-12-12 22:58:26 +0100

    Órai munka kiadása

---
+++ Primszam/PrimszamProg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Kérjünk be egy számot és írjuk ki, hogy prímszám-e!
+ */
+
+namespace Primszam
+{
+    internal class PrimszamProg
+    {
+        static void Main(string[] args)
+        {
+            int szam,
+                oszto;
+
+            bool prim = true;
+
+            string msg;
+
+            Console.WriteLine("Adjon meg egy egész számot!");
+            szam = Convert.ToInt32(Console.ReadLine());
+
+            oszto = szam / 2;
+
+            while (oszto > 3 && prim)
+            {
+                if (szam % oszto == 0) prim = false;
+                oszto--;
+            }
+
+            msg = prim ? "prím" : "nem prím";
+
+            Console.WriteLine($"A {szam} {msg}szám.");
+
+            Console.ReadKey();
+        }
+    }
+}
