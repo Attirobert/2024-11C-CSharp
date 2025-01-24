23b93148858daeb09511852acd2922d2eae62c5f
Attirobert <karobatti@gmail.com>
2025-01-24 13:20:46 +0100

    Órai

---
+++ Tombs/Tombs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tombs
+{
+    class Tombs
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
