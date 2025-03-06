f306e1c65f824ced8c242dc8f933f26141e61ce8
Attirobert <robatti@gmail.com>
2025-03-06 21:41:01 +0100

    FájlKezeles elkezdve

---
+++ Fajlkezeles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Állítsunk elő 5 db egész véletlen számot a [0, 100] tartományból, majd 
+ *  - írjuk ki a képernyőre,
+ *  - kérjünk be egy érvényes fájl nevet kiterjesztés nélkül, adjuk hozzá a "txt" kiterjesztést,
+ *  - írjuk ki ezeket a számokat a megadott fájlba
+ */
+
+namespace Fajlkezeles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Változók
+            string fajlNev,
+                kit = ".txt";
+
+            int[] tomb = new int[5];
+
+            // Beolvassuk a fájlnevet
+        }
+    }
+}
