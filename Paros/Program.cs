ba66ba409c903fe30f1777a50fab4484ae89cbf6
Attirobert <karobatti@gmail.com>
2025-03-12 14:31:42 +0100

    2025.03.12 órai anyag

---
+++ Paros/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paros
+{ //Beolvasunk konzolról egy számot, és eldöntjük, hogy páros-e vagy páratlan-e.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Változók
+            int p;
+            bool paros = false;
+
+            //Beolvassuk
+            p = beolvas("Adjuk meg a számot. ");
+            
+            // Eldöntjük, hogy páros-e
+            paros = dontes(p);
+
+            // Kiíratás
+            kiir(paros);
+        }
+
+        private static void kiir(bool paros)
+        {
+            if (paros)
+                Console.WriteLine($"A szám páros");
+            else
+                Console.WriteLine("A szám páratlan");
+
+            Console.ReadKey();
+        }
+
+        private static bool dontes(int p)
+        {
+            if (p % 2 == 0)
+                return true;
+            else return false;
+        }
+
+        private static int beolvas(string v)
+        {
+            Console.WriteLine(v);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Tegla/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tegla
+{ //Beolvassunk 3 egész számot, amelyek a téglatest oldalai, majd kiszámoljuk a téglatest felszínét, térgoatát, majd kiiratjuk az eredményt
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Változók
+            int a,
+                b,
+                c,
+                terfogat,
+                felszin;
+
+            //Bekérés
+            a = Bekeres("Bekérjük az A-t");
+            b = Bekeres("Bekérjük a B-t");
+            c = Bekeres("Bekérjük a C-t");
+
+            //Kiszámitás
+            terfogat = Terfogat(a ,b, c);
+            felszin = Felszin(a, b, c);
+
+            //Kiiras
+            Kiiras(terfogat, felszin);
+
+
+
+
+        }
+
+        private static void Kiiras(int terfogat, int felszin)
+        {
+            Console.WriteLine($"A téglatest felszine: {felszin}, a térfogata: {terfogat}");
+            Console.ReadKey();
+        }
+
+        private static int Felszin(int a, int b, int c)
+        {
+            int y = 2*(a * b + a * c + b * c);
+            return y;
+        }
+
+        private static int Terfogat(int a, int b, int c)
+        {
+            int y = a * b * c;
+            return y;
+        }
+
+        private static int Bekeres(string v)
+        {
+            int x = 0;
+            Console.WriteLine(v);
+            x=Convert.ToInt32(Console.ReadLine());
+            
+            return x;
+        }
+    }
+}
