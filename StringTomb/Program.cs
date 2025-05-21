4643b44fdaed02709937a98a49ee233639d6f383
Attirobert <karobatti@gmail.com>
2025-05-21 14:46:11 +0200

    Órai munka

---
+++ StringTomb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTomb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Változó
+            string s = "A C# kiváló programozási nyelv";
+
+            for (int i = 0; i < 5; i++)
+            {
+                char x =Kivalaszt(s);
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static char Kivalaszt(string s)
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(0, s.Length);
+            return s[i];
+        }
+    }
+}
---
+++ Tombn01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Létrehozunk egy 10 elemű tömböt és feltöltjük véletlen számokkal (egész)
+ * Kiírjuk a számokat.
+ * 
+ * */
+
+namespace Tombn01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Változók
+            int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Deklaráció 1
+            int[] ints2;    // Deklaráció 2
+
+            ints2 = new int[10];
+
+            Console.WriteLine(ints2.Length);
+            Console.WriteLine(ints.Length);
+
+            TombFeltoltes(ints2);
+        }
+
+        private static void TombFeltoltes(int[] ints2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
